d73f703b97ef0672e62854b3a90b9757775cc0d3
Julian P Samaroo <jpsamaroo@gmail.com>
2026-05-18 11:12:27 -0700

    Vendor ConcurrentStack, remove ConcurrentCollections dep

---
+++ src/stack.jl
@@ -0,0 +1,50 @@
+# Adapted from ConcurrentCollections.jl/src/stack.jl
+
+mutable struct TSNode{T}
+    value::T
+    @atomic next::Union{TSNode{T},Nothing}
+end
+
+TSNode{T}(value::T) where {T} = TSNode{T}(value, nothing)
+
+mutable struct ConcurrentStack{T}
+    @atomic next::Union{TSNode{T},Nothing}
+end
+
+ConcurrentStack{T}() where {T} = ConcurrentStack{T}(nothing)
+
+function Base.push!(stack::ConcurrentStack{T}, v) where {T}
+    v = convert(T, v)
+    node = TSNode{T}(v)
+
+    next = @atomic stack.next
+    while true
+        @atomic node.next = next
+        next, ok = @atomicreplace(stack.next, next => node)
+        ok && break
+    end
+
+    return stack
+end
+
+function maybepop!(stack::ConcurrentStack)
+    while true
+        node = @atomic stack.next
+        node === nothing && return nothing
+
+        next = @atomic node.next
+        next, ok = @atomicreplace(stack.next, node => next)
+        if ok
+            return Some(node.value)
+        end
+    end
+end
+
+function Base.pop!(stack::ConcurrentStack)
+    r = maybepop!(stack)
+    if r === nothing
+        error("stack is empty")
+    else
+        return something(r)
+    end
+end
